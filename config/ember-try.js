bed9eb969877b0c0745f7f648280293d1fbe17d4
simonihmig <simon.ihmig@kaliber5.de>
2021-11-12 10:19:02 +0100

    Fix CI scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,18 +8,10 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.20',
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.5',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
+            'ember-source': '~3.28.5',
           },
         },
       },
